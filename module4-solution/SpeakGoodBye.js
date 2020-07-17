947c1d32c01f7d35328a4ba83a06982956ebfcab
poojaverma <poojaverma05@gmail.com>
2020-07-17 11:01:02 +0530

    Add files via upload

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,15 @@
+﻿(function (window) {
+
+    var byeSpeaker=new Object();
+
+ var speakWord = "Good Bye";
+
+
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " +name);
+}
+
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,16 @@
+﻿(function (window) {
+
+    var helloSpeaker=new Object();
+
+ 
+var speakWord = "Hello";
+
+ 
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+
+
+window.helloSpeaker = helloSpeaker;
+
+})(window);
