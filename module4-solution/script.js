27898e5e683ea925b65df48bc02aad115c97d828
poojaverma <poojaverma05@gmail.com>
2020-07-17 11:22:28 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,20 +1,26 @@
+/*
+Solution of assignment 4:
+Expected output:
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+*/
 
-(function () {
-
-var names = ["Yaazi", "Jacob", "Jenny", "Jason", "Paul", "Frank", "Larry", "Pooja", "Mehak", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
-  }
-  else {
-    helloSpeaker(names[i]);
-  }
-}
-
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
 })();
-
-
-
